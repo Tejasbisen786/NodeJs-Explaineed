58f421347d35be3799fd627839d20d04bc5e6520
Tejasbisen786 <bisentejas@gmail.com>
2024-03-23 10:48:30 +0530

    intro to node js

---
+++ 01.js
@@ -0,0 +1,10 @@
+// ***What is NODE JS ***
+
+// js -> Javascript engiine help to execute js code , engine present inside the browser
+// chrome ->  v8 Enginee [Most Popular] -> Ryan Dahl - inverntor OF NOdde js
+// safari-> spider
+
+// -> You can run js outside of the brower
+// -> you can create web servers in javascript langugae 
+
+// ** Node Js is Runtime Environment For Javascipt 
---
+++ 02.js
@@ -0,0 +1,11 @@
+// ****Installation of Node js ***
+
+// LTS : Stable Release [ Production ]
+// CUrrent :  BETA Version
+
+// EVEN-> Current  stable bugproof version denote
+// ODD -> unstable version denote
+
+
+// npm -> node package manager 
+// npm -V -> check the version of node js 
