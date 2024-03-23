0d2c9a499e9f9a755e2e0015032f35d87a0d0746
Tejasbisen786 <bisentejas@gmail.com>
2024-03-23 11:13:26 +0530

    File Handling in Node Js USing Fs Module

---
+++ 03-HelloWorld/file.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+// sync async ->  blocking and unblocking request
+
+// ** Creating a File
+//Sync
+// fs.writeFileSync("./test.txt", "Tejas Bisen");
+//Async
+// fs.writeFile("./test1.txt", "Tejas Bisen Async File", (err) => {});
+
+// **reading file
+// sync -> returning result
+// const rsult = fs.readFileSync("./contact.txt","utf-8")
+// console.log(rsult)
+
+// expecting and defining callback func
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// ** Append File
+// new Date().getDate().toLocaleString()
+fs.appendFileSync("./test.txt", ` ${Date.now()}hey Tejas\n`);
+
+
+// copy file
+fs.cpSync("./contact.txt", './copyContaxt.txt')
+
+// unlink file / Deleting A File
+
+fs.unlinkSync("./copyContaxt.txt")
+
+console.log(fs.statSync("./test.txt"));
