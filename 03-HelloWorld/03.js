985a581f5617d5da95b2003c6e3922c4e3cea720
Tejasbisen786 <bisentejas@gmail.com>
2024-03-23 10:49:10 +0530

    hello in node js | module in node js

---
+++ 03-HelloWorld/03.js
@@ -0,0 +1,15 @@
+// *** Hello World JS ***
+// console.log("Tejas Bisen")
+// console.log() // Global Object []
+// removing window relation task including dom
+// npm init -> for crating a package.json file
+// it is configuration file
+
+// **** Module in Node Js ********
+
+// const {add,sub} = require("./math"); // ** user-Defined Module
+// const fs =require("fs") // ** bult in module
+// const cypt= require("crypto")
+
+console.log(add(2, 3));
+console.log(sub(5, 3));
