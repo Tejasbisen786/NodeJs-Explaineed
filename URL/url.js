a82e7cc80ed4923be2395422c1181ad18d0bf6ad
Tejasbisen786 <bisentejas@gmail.com>
2024-03-25 12:19:42 +0530

    url handiling in node js

---
+++ URL/url.js
@@ -0,0 +1,14 @@
+// *URL : uniform resource locator
+
+// *https://bisentejas.dev/
+
+// https://-> Secure Protocol
+//  bisentejas.dev ->  Domain
+//  '/' -> Path [Root Path] : Homepage
+//  '/contact' : Assemble Path
+//  '/contact/name/user1' -> Nested Path
+
+
+// *Query parameter
+
+// *bisentejas.dev/about ? userId=1&a=2
