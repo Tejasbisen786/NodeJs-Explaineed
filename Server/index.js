7deb7bdbc837b48ab186cba79e0dc5387d777f0a
Tejasbisen786 <bisentejas@gmail.com>
2024-03-25 12:19:00 +0530

    url modified

--- Server/index.js
+++ Server/index.js
@@ -1,35 +1,33 @@
 // creating http server
-
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  const date = new Date();
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} ${req.url} : New Request Received \n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
 
-  const log = `
-  ${date.toDateString()} 
-  ${date.getSeconds()} 
-  ${req.url} 
-  
-  : New Request Received\n`;
   fs.appendFile("log.text", log, (err, result) => {
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("HomePage");
         break;
       case "/about":
-        res.end("About Page");
+        const userName = myUrl.query.myName;
+        res.end(` MyName is ${userName}`);
         break;
+
       case "/contact":
         res.end("Contact Page");
         break;
+
       default:
         res.end("404");
         break;
     }
   });
-
-  res.end("Hello From Server");
 });
 
 myServer.listen(8000, () => {
